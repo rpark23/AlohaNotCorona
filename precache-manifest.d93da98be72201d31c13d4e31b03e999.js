08a63da7e45c68dbf3664c5325062f910d2a30eb
rpark23 <ryanparkusa@yahoo.com>
2020-06-20 23:33:17 -1000

    Updates

---
+++ precache-manifest.d93da98be72201d31c13d4e31b03e999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4349b1f0a79b14405b2cadf7f9f52a5e",
+    "url": "/AlohaNotCorona/index.html"
+  },
+  {
+    "revision": "34e48894de995f24418e",
+    "url": "/AlohaNotCorona/static/css/main.4267b438.chunk.css"
+  },
+  {
+    "revision": "1a577672c8cae63e07a5",
+    "url": "/AlohaNotCorona/static/js/2.dc41017d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlohaNotCorona/static/js/2.dc41017d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34e48894de995f24418e",
+    "url": "/AlohaNotCorona/static/js/main.5413b109.chunk.js"
+  },
+  {
+    "revision": "bbbb8d9655ae2ea608ff",
+    "url": "/AlohaNotCorona/static/js/runtime-main.e7528075.js"
+  }
+]);
